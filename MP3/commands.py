853072c8c5b33da52a1de1e772778ba9f491dcf8
Venkatesh Palanivel <vp35@illinois.edu>
2024-10-06 14:24:09 -0500

    MP3 task 2

--- MP3/commands.py
+++ MP3/commands.py
@@ -8,28 +8,43 @@ NetIDs_str = " ".join(NetIDs)
 
 # TODO: Clone your GitHub repository
 ! git clone [Your GitHub Link]
-%cd [Your GitHub Repo]
+%cd [Your GitHub Repo]/MP3
 
 # Set up requirements for dataset generation
+! bash -x setup_dataset.sh
 
 # dataset generation
-! python3 MP3/codenet_dataset_generation.py {NetIDs_str} |& tee codenet_dataset_generation.log
+! python3 codenet_dataset_generation.py {NetIDs_str} |& tee codenet_dataset_generation.log
 
 seed = "<your_seed>"
 # TODO: Replace the file path of selected_codenet_[seed].jsonl generated in previous step
 input_dataset = "selected_codenet_" + seed + ".jsonl"
 
 # Set up requirements for model prompting
-! bash -x MP1/setup_models.sh
+! bash -x setup_models.sh
 
 task_1_response = "task_1_response_" + seed + ".jsonl"
 task_1_evaluation = "task_1_evaluation_" + seed + ".jsonl"
 
 # Task 1
-! python3 MP3/task_1.py {input_dataset} {task_1_response} "True" |& tee task_1.log
-! python3 MP3/evaluation_translation.py {task_1_response} {task_1_evaluation} |& tee task_1_evaluation.log
+! python3 task_1.py {input_dataset} {task_1_response} "True" |& tee task_1.log
+! python3 evaluation_translation.py {task_1_response} {task_1_evaluation} |& tee task_1_evaluation.log
 
 # Task 2
+
+# TODO: Replace the file path of selected_humaneval_[seed].jsonl generated in MP1
+humaneval_input_dataset = "selected_humaneval_" + seed + ".jsonl"
+
+# Prompt the models, you can modify `MP3/task_2.py`
+# The {input_dataset} is the JSON file consisting of 20 unique programs for your group that you generated in MP1 (selected_humaneval_[seed].jsonl)
+! python3 task_2_dataset_generation.py {humaneval_input_dataset}
+
+task_2_predict_json = "task_2_predict_" + seed + ".jsonl"
+task_2_localize_json = "task_2_localize_" + seed + ".jsonl"
+humanevalpack_input_dataset = "selected_humanevalpack_" + seed + ".jsonl"
+
+! python3 task_2.py {humanevalpack_input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_predict_json} "False" |& tee task_2_predict.log
+! python3 task_2.py {humanevalpack_input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_localize_json} "True" |& tee task_2_localize.log
 
 %cd ..
 
